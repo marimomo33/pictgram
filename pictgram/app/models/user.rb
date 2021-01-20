db11366ccb4e421f326b2278ff1c817dfdcf6533
EC2 Default User <ec2-user@ip-172-31-37-31.us-east-2.compute.internal>
2021-01-20 12:20:05 +0000

    rails4

---
+++ pictgram/app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+    
+    VALID_EMAIL_REGEX=/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/  #メールアドレスの型
+    VALID_PASSWORD_REGEX=/\A(?=.*?[a-z])[a-z\d]{8,32}+\z/               #アルファベットと数字の混合
+  validates :name, presence: true,length:{maximum: 15}                  #nameの最大は15
+  validates :email, presence: true,format:{with:VALID_EMAIL_REGEX}      #emailはメールアドレスの型で
+ 
+  has_secure_password validations: false
+  
+  validates_confirmation_of :password, allow_blank: true, on: :update   #??
+  validates :password, presence: true,                                  
+  length:{ minimum: 8, maximum: 32},format:{with:VALID_PASSWORD_REGEX}  #passwordの長さは8～32かつアルファベットと数字の混合
+  
+  
+
+  
+end
