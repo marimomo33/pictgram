c15e100147458182e8e027fbff0820e5b5e1b5b2
EC2 Default User <ec2-user@ip-172-31-37-31.us-east-2.compute.internal>
2021-02-02 14:34:19 +0000

    kadai5

--- pictgram/app/models/user.rb
+++ pictgram/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     
-    VALID_EMAIL_REGEX=/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/  #メールアドレスの型
+    VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  #メールアドレスの型
     VALID_PASSWORD_REGEX=/\A(?=.*?[a-z])[a-z\d]{8,32}+\z/               #アルファベットと数字の混合
   validates :name, presence: true,length:{maximum: 15}                  #nameの最大は15
   validates :email, presence: true,format:{with:VALID_EMAIL_REGEX}      #emailはメールアドレスの型で
